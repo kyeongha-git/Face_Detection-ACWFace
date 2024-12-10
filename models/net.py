640737c2392e2df2db28022601d5416bbf3a7f87
Kyeong Ha Hwang <168719498+kyeongha-git@users.noreply.github.com>
2024-12-10 18:28:13 +0900

    final ACWFACE.py

--- models/net.py
+++ models/net.py
@@ -12,7 +12,7 @@ import math
 
 def conv_bn(inp, oup, stride = 1, leaky = 0):
     return nn.Sequential(
-        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        DeformableConv2d(inp, oup, 3, stride, 1, bias=False),
         # nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
         nn.BatchNorm2d(oup),
         nn.LeakyReLU(negative_slope=leaky, inplace=True)
@@ -20,14 +20,14 @@ def conv_bn(inp, oup, stride = 1, leaky = 0):
     
 def conv_bn_no_relu(inp, oup, stride):
     return nn.Sequential(
-        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        DeformableConv2d(inp, oup, 3, stride, 1, bias=False),
         # nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
         nn.BatchNorm2d(oup),
     )
     
 def conv_bn1X1(inp, oup, stride, leaky=0):
     return nn.Sequential(
-        nn.Conv2d(inp, oup, 1, stride, padding=0, bias=False),
+        DeformableConv2d(inp, oup, 1, stride, padding=0, bias=False),
         nn.BatchNorm2d(oup),
         nn.LeakyReLU(negative_slope=leaky, inplace=True)
     )
